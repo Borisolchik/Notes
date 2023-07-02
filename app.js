3f01439d1593243d1a386af2b50c3b5008f73eb3
Olga Boris <olya_boris97@mail.ru>
2023-07-02 23:36:20 +0300

    added functional

---
+++ app.js
@@ -0,0 +1,46 @@
+const inputElement = document.getElementById('title');
+const createBtn = document.getElementById('create');
+const listElement = document.getElementById('list');
+
+const notes = [
+    {
+        title: 'Посетить meetup frontend-разработчиков',
+        completed: false
+    },
+    {
+        title: 'Созвон с командой',
+        completed: true
+    }
+    ];
+
+function render() {
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+    }
+}
+
+render();
+
+createBtn.onclick = function () {
+    if (inputElement.value.length === 0) {
+        return;
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false
+    }
+    listElement.insertAdjacentHTML(
+        'beforeend',
+        getNoteTemplate(newNote));
+    inputElement.value = '';
+}
+
+function getNoteTemplate(note) {
+    return `<li class="list-group-item d-flex justify-content-between align-items-center">
+          <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+          <span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}">&check;</span>
+            <span class="btn btn-small btn-danger">&times;</span>
+          </span>
+        </li>`
+}
