06a193c993b39bceb2ce780166c6ec932784337e
Olga Boris <olya_boris97@mail.ru>
2023-07-03 15:29:48 +0300

    update

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const notes = [
 
 function render() {
     listElement.innerHTML = '';
+    if (notes.length === 0) {
+        listElement.innerHTML = '<p>Нет заметок</p>'
+    }
     for (let i = 0; i < notes.length; i++) {
         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));
     }
@@ -42,8 +45,8 @@ listElement.onclick = function (event) {
 
         if (type === 'toggle') {
             notes[index].completed = !notes[index].completed;
-        } else  if (type === 'remove') {
-
+        } else if (type === 'remove') {
+            notes.splice(index, 1);
         }
 
         render();
@@ -55,7 +58,7 @@ function getNoteTemplate(note, index) {
           <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
           <span>
             <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
-            <span class="btn btn-small btn-danger" data-type="remove">&times;</span>
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
           </span>
         </li>`
 }
